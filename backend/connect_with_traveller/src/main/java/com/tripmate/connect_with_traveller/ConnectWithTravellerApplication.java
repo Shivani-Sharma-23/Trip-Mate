55b3adc2c36435e7ab192255f295b9b548f09323
Shivani-Sharma-23 <shivanisharma232005@gmail.com>
2026-02-11 11:28:53 +0530

    First Commit

---
+++ backend/connect_with_traveller/src/main/java/com/tripmate/connect_with_traveller/ConnectWithTravellerApplication.java
@@ -0,0 +1,13 @@
+package com.tripmate.connect_with_traveller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectWithTravellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectWithTravellerApplication.class, args);
+	}
+
+}
---
+++ backend/connect_with_traveller/src/test/java/com/tripmate/connect_with_traveller/ConnectWithTravellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripmate.connect_with_traveller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectWithTravellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
